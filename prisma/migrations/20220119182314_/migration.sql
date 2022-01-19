0937c82c0e54975250cd3b61044f387368a9cf24
Maxime <maxime.derame@hotmail.fr>
2022-01-19 19:25:06 +0100

    add migrations

---
+++ prisma/migrations/20220119182314_/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Bar" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Bar_pkey" PRIMARY KEY ("id")
+);
---
+++ prisma/migrations/20220119182448_/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Permanence" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Permanence_pkey" PRIMARY KEY ("id")
+);
