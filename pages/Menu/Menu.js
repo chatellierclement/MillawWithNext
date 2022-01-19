94308234f386fec22e5895336c526d87e334c284
Clement CHATELLIER <chatellierclement44@gmail.com>
2022-01-19 21:25:01 +0100

    navLink to link

--- pages/Menu/Menu.js
+++ pages/Menu/Menu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from 'next/link'
 import { NavLink } from "react-bootstrap";
 
 export default class Menu extends React.Component { 
@@ -31,47 +32,27 @@ export default class Menu extends React.Component {
             <div className="navbar-collapse">
                 <ul className="navbar-nav mb-2">
                     <li className="nav-item">
-                        <NavLink href="/Calendar/calendar">Planning</NavLink>
+                        <Link  href="/Calendar/calendar"><a className="test">Planning</a></Link>
                     </li>
-                    <li className="nav-item">
-                        <NavLink href="/admin/utilisateur">Utilisateur</NavLink>
+                    <li className="nav-item">                    
+                        <Link href="/admin/utilisateur"><a>Utilisateur</a></Link>
                     </li>
-                    <li className="nav-item">
-                        <NavLink href="/admin/permanence">Permanence</NavLink>
+                    <li className="nav-item">                    
+                        <Link href="/admin/permanence"><a>Permanence</a></Link>
                     </li>
-                    <li className="nav-item">
-                        <NavLink href="/admin/barreau">Barreau</NavLink>
+                    <li className="nav-item">                    
+                        <Link href="/admin/barreau"><a>Barreau</a></Link>
                     </li>
-
                 </ul>
             </div>
         </div>
-        {/* <div className="vertical-nav" id="sidebar">
-            <div className="py-4 px-3 mb-4 media">
-                <div className="d-flex align-items-center">
-                    <div className="media-body">
-                        <h2 className="m-0">Millaw</h2>
-                        <h4 className="m-0">{this.state.user.firstName} {this.state.user.lastName}</h4>
-                        <p className="font-weight-light text-muted mb-0">{this.state.user.role.libelle}</p>
-                    </div>
-                </div>
-            </div>
-
-            <ul className="nav flex-column mb-0">
-                <li className="nav-item activeLi">
-                    <Link className="nav-link text-white" href="/Calendar/calendar">Planning</Link>
-                </li>
-                <li className="nav-item">                    
-                    <Link className="nav-link text-white" href="/admin/utilisateur">Utilisateur</Link>
-                </li>
-                <li className="nav-item">                    
-                    <Link className="nav-link text-white" href="/admin/permanence">Permanence</Link>
-                </li>
-                <li className="nav-item">                    
-                    <Link className="nav-link text-white" href="/admin/barreau">Barreau</Link>
-                </li>
-            </ul>          
-        </div> */}
+        
+        <style>
+            {`.test {
+                color:red;
+            }
+        `}
+        </style>
         </>
     );
   }
