1c458af2b9f525ae64e19fdcfbcb9dd520107f37
Maxime <maxime.derame@hotmail.fr>
2022-01-28 20:46:55 +0100

    add document page

--- pages/app/Menu/Menu.js
+++ pages/app/Menu/Menu.js
@@ -53,8 +53,7 @@ export default function Menu() {
                     <li className="nav-item">                    
                         <Link href="/app/documents">
                             <a className="nav-link">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-                                <span className="ms-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>                                <span className="ms-2">
                                     Documents
                                 </span> 
                             </a>
--- pages/app/documents.js
+++ pages/app/documents.js
@@ -1,5 +1,102 @@
+import Image from 'next/image'
+
 export default function Documents() {
+
+    function eventClick(row = null) {    
+
+    }
+
     return (
-        <>sdkf</>
+        <>
+        <div className="px-3 px-xxl-5 py-3 py-lg-4 border-bottom border-gray-200 after-header">
+            <div className="container-fluid px-0">
+                <div className="row align-items-center">
+                    <div className="col">
+                    <h1 className="h2 mb-0">Documents</h1>
+                    </div>
+
+                    <div className="col-auto d-flex align-items-center my-2 my-sm-0">
+                        <a href="#" onClick={eventClick} className="btn btn-lg btn-darner px-3 me-2 me-md-3">
+                            <span className="ps-1">Ajouter</span>
+                            <svg className="ms-4" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
+                                <rect data-name="Icons/Tabler/Add background" width="14" height="14" fill="none"></rect>
+                                <path d="M6.329,13.414l-.006-.091V7.677H.677A.677.677,0,0,1,.585,6.329l.092-.006H6.323V.677A.677.677,0,0,1,7.671.585l.006.092V6.323h5.646a.677.677,0,0,1,.091,1.348l-.091.006H7.677v5.646a.677.677,0,0,1-1.348.091Z" fill="#1e1e1e"></path>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>  
+
+        <div className="p-3 p-xxl-5">
+            <div className="container-fluid px-0 pb-1 pb-md-4">
+                <div className="border-bottom border-gray-200 pb-2 pb-xl-5 mb-2 mb-xl-5">
+                    <h5 className="font-weight-semibold text-black-600 mb-lg-4 pb-1">Ajoutés récemments</h5>
+                    <div className="row">
+                        <div className="col-6 col-md-4 col-lg-6 col-xl-4 has-xxl-5 mb-3 mb-lg-4">
+                            <figure className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
+                                <figcaption className="text-center">
+                                    <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
+                                    <h6 className="font-weight-semibold open-sans-font mt-3">CERFA 3654</h6>
+                                    <p className="text-gray-600 fs-11 lh-base">Uploaded today</p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div className="col-6 col-md-4 col-lg-6 col-xl-4 has-xxl-5 mb-3 mb-lg-4">
+                            <figure className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
+                                <figcaption className="text-center">
+                                    <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
+                                    <h6 className="font-weight-semibold open-sans-font mt-3">AJ</h6>
+                                    <p className="text-gray-600 fs-11 lh-base">Uploaded today</p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div className="col-6 col-md-4 col-lg-6 col-xl-4 has-xxl-5 mb-3 mb-lg-4">
+                            <figure className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
+                                <figcaption className="text-center">
+                                    <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
+                                    <h6 className="font-weight-semibold open-sans-font mt-3">CERFA 8521</h6>
+                                    <p className="text-gray-600 fs-11 lh-base">Uploaded today</p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="border-bottom border-gray-200 pb-2 pb-xl-5 mb-2 mb-xl-5">
+                    <h5 className="font-weight-semibold text-black-600 mb-lg-4 pb-1">Documents</h5>
+                    <div className="row">
+                        <div className="col-6 col-md-4 col-lg-6 col-xl-4 has-xxl-5 mb-3 mb-lg-4">
+                            <figure className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
+                                <figcaption className="text-center">
+                                    <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
+                                    <h6 className="font-weight-semibold open-sans-font mt-3">CERFA 3654</h6>
+                                    <p className="text-gray-600 fs-11 lh-base">Uploaded today</p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div className="col-6 col-md-4 col-lg-6 col-xl-4 has-xxl-5 mb-3 mb-lg-4">
+                            <figure className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
+                                <figcaption className="text-center">
+                                    <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
+                                    <h6 className="font-weight-semibold open-sans-font mt-3">AJ</h6>
+                                    <p className="text-gray-600 fs-11 lh-base">Uploaded today</p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div className="col-6 col-md-4 col-lg-6 col-xl-4 has-xxl-5 mb-3 mb-lg-4">
+                            <figure className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
+                                <figcaption className="text-center">
+                                    <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
+                                    <h6 className="font-weight-semibold open-sans-font mt-3">CERFA 8521</h6>
+                                    <p className="text-gray-600 fs-11 lh-base">Uploaded today</p>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
     )
 }
