b83ec5fb8dc27bab1a18ac1856754e751e678848
Maxime <maxime.derame@hotmail.fr>
2022-02-09 21:19:25 +0100

    wizard step improvements

--- components/StepForm/LawyerStep.tsx
+++ components/StepForm/LawyerStep.tsx
@@ -1,6 +1,20 @@
 export default function LawyerStep() {
     return (
         <>
+            <div className="mb-4 mb-xl-5">
+                <label className="form-label form-label-lg" htmlFor="ProjectName">Choix d&apos;un avocat</label>
+
+                <select className="form-label form-label-lg" id="ProjectName">
+                    <option>John Doe</option>
+                    <option>Clément Chatellier</option>
+                </select>
+            </div>
+
+            <div className="mb-4 mb-xl-5">
+                <label className="form-label form-label-lg" htmlFor="ProjectName">Avocat de suivi</label>
+
+                <input type="text" value="John Doe" disabled id="ProjectName" className="form-control form-control-xl" />
+            </div>
         </>
     )
 }
