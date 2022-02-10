37954f8f34c6a726c8848a36119f42adcc1a3454
Clement CHATELLIER <chatellierclement44@gmail.com>
2022-02-10 16:34:20 +0100

    documents get

--- pages/app/documents.js
+++ pages/app/documents.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { Modal, Button } from 'react-bootstrap'
 import axios from 'axios';
+import moment from 'moment';
 import {NotificationContainer, NotificationManager} from 'react-notifications';
 import { FileUploader } from "react-drag-drop-files";
 
@@ -9,11 +10,29 @@ export default function Documents() {
     const [show, setShow] = useState(false); 
     const [showAddDoc, setShowAddDoc] = useState(false); 
     const [selectedFile, setSelectedFile] = useState(null); 
+    const [selectedUrl, setSelectedUrl] = useState(null); 
+    const [documents, setDocuments] = useState(null); 
     const fileTypes = ["PDF"];
 
+    useEffect(() => {
+        getDocuments()  
+    }, [])
+
+    //Initialisation des Events
+    async function getDocuments() {
+        await axios.get('/api/document')
+        .then(function (response) { 
+            setDocuments(response.data);
+        }) 
+        .catch(function (error) { 
+            console.log(error); 
+        }) 
+    }
+
     //Ouverture/Fermeture de la modal
-    function openCloseModal(arg = false) {
+    function openCloseModal(arg = false, url = null) {
         setShow(arg);
+        setSelectedUrl(url)
     }
     //Ouverture/Fermeture de la modal
     function openCloseModalAddDocument(arg = false) {
@@ -24,10 +43,6 @@ export default function Documents() {
         setSelectedFile(event)
     }
 
-    function eventDeleteDoc() {
-        setSelectedFile(null)
-    }
-
     async function eventClick() {         
 
         const data = new FormData() 
@@ -35,7 +50,7 @@ export default function Documents() {
         await axios.post('http://localhost:8000/upload', data) 
         .then(function (response) {
             let newDoc = {
-                name: 'test',
+                name: response.data.filename.split("-")[1],
                 url: "documents/" + response.data.filename,
                 user_id: 1
             }
@@ -43,6 +58,7 @@ export default function Documents() {
             axios.post('/api/document', newDoc) 
             .then(function (response) {
                 NotificationManager.success("success", "Le document est enregistré avec succès.", 3000)
+                getDocuments()
             }) 
         }) 
         .catch(function (error) { 
@@ -50,14 +66,6 @@ export default function Documents() {
           })  
           
         openCloseModalAddDocument(false)
-    }
-
-    function openDocument() {    
-        openCloseModal(true)
-    }
-
-    function openAddDocument() {    
-        openCloseModalAddDocument(true)
     }
 
     return (
@@ -70,7 +78,7 @@ export default function Documents() {
                     </div>
 
                     <div className="col-auto d-flex align-items-center my-2 my-sm-0">                        
-                        <a href="#" onClick={openAddDocument} className="btn btn-lg btn-darner px-3 me-2 me-md-3">
+                        <a href="#" onClick={() => openCloseModalAddDocument(true)} className="btn btn-lg btn-darner px-3 me-2 me-md-3">
                             <span className="ps-1">Ajouter un document</span>
                             <svg className="ms-4" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
                                 <rect data-name="Icons/Tabler/Add background" width="14" height="14" fill="none"></rect>
@@ -85,70 +93,26 @@ export default function Documents() {
         <div className="p-3 p-xxl-5">
             <div className="container-fluid px-0 pb-1 pb-md-4">
                 <div className="border-bottom border-gray-200 pb-2 pb-xl-5 mb-2 mb-xl-5">
-                    <h5 className="font-weight-semibold text-black-600 mb-lg-4 pb-1">Ajoutés récemments</h5>
-                    <div className="row">
-                        <div className="col-6 col-md-4 col-lg-6 col-xl-4 has-xxl-5 mb-3 mb-lg-4">
-                            <figure onClick={openDocument} className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
-                                <figcaption className="text-center">
-                                    <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
-                                    <h6 className="font-weight-semibold open-sans-font mt-3">CERFA 3654</h6>
-                                    <p className="text-gray-600 fs-11 lh-base">Uploaded today</p>
-                                </figcaption>
-                            </figure>
-                        </div>
-                        <div className="col-6 col-md-4 col-lg-6 col-xl-4 has-xxl-5 mb-3 mb-lg-4">
-                            <figure className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
-                                <figcaption className="text-center">
-                                    <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
-                                    <h6 className="font-weight-semibold open-sans-font mt-3">AJ</h6>
-                                    <p className="text-gray-600 fs-11 lh-base">Uploaded today</p>
-                                </figcaption>
-                            </figure>
-                        </div>
-                        <div className="col-6 col-md-4 col-lg-6 col-xl-4 has-xxl-5 mb-3 mb-lg-4">
-                            <figure className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
-                                <figcaption className="text-center">
-                                    <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
-                                    <h6 className="font-weight-semibold open-sans-font mt-3">CERFA 8521</h6>
-                                    <p className="text-gray-600 fs-11 lh-base">Uploaded today</p>
-                                </figcaption>
-                            </figure>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="border-bottom border-gray-200 pb-2 pb-xl-5 mb-2 mb-xl-5">
                     <h5 className="font-weight-semibold text-black-600 mb-lg-4 pb-1">Documents</h5>
                     <div className="row">
-                        <div className="col-6 col-md-4 col-lg-6 col-xl-4 has-xxl-5 mb-3 mb-lg-4">
-                            <figure className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
-                                <figcaption className="text-center">
-                                    <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
-                                    <h6 className="font-weight-semibold open-sans-font mt-3">CERFA 3654</h6>
-                                    <p className="text-gray-600 fs-11 lh-base">Uploaded today</p>
-                                </figcaption>
-                            </figure>
-                        </div>
-                        <div className="col-6 col-md-4 col-lg-6 col-xl-4 has-xxl-5 mb-3 mb-lg-4">
-                            <figure className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
-                                <figcaption className="text-center">
-                                    <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
-                                    <h6 className="font-weight-semibold open-sans-font mt-3">AJ</h6>
-                                    <p className="text-gray-600 fs-11 lh-base">Uploaded today</p>
-                                </figcaption>
-                            </figure>
-                        </div>
-                        <div className="col-6 col-md-4 col-lg-6 col-xl-4 has-xxl-5 mb-3 mb-lg-4">
-                            <figure className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
-                                <figcaption className="text-center">
-                                    <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
-                                    <h6 className="font-weight-semibold open-sans-font mt-3">CERFA 8521</h6>
-                                    <p className="text-gray-600 fs-11 lh-base">Uploaded today</p>
-                                </figcaption>
-                            </figure>
-                        </div>
+                        {documents &&
+                            documents.map((document, index) => (
+                                <div className="col-3 col-md-3 col-lg-3 col-xl-3 has-xxl-3 mb-3 mb-lg-3">
+                                    <figure onClick={() => openCloseModal(true, "/" + document.url)} className="p-2 rounded-6 shadow-dark-80 bg-white mb-0 h-100 border border-gray-200">
+                                        <figcaption className="text-center">
+                                            <Image src="/pdf.svg" alt="PDF" height={40} width={33} />
+                                            <h6 className="font-weight-semibold open-sans-font mt-3">{document.name}</h6>
+                                            <p className="text-gray-600 fs-11 lh-base">{moment(document.createdAt, "YYYY-MM-DD hh:mm:ss").fromNow()}</p>
+                                        </figcaption>
+                                    </figure>
+                                </div>
+                            ))  
+                        }  
+                        {!documents || documents.length === 0 && 
+                            <p>Aucun document déposé.</p>
+                        }                
                     </div>
-                </div>
+                </div>                
             </div>
         </div>
 
@@ -159,7 +123,7 @@ export default function Documents() {
                 <Modal.Title>PDF</Modal.Title>
             </Modal.Header>
             <Modal.Body>              
-                 <iframe src="/documents/doc.pdf"></iframe>                      
+                 <iframe src={selectedUrl}></iframe>                      
             </Modal.Body>    
         </Modal>
 
@@ -172,8 +136,11 @@ export default function Documents() {
                 <p>
                     <span>{selectedFile ? `Fichier : ${selectedFile.name}` : "Aucun fichier déposé"}</span>
                     {selectedFile &&
-                        <Button variant="danger" className="float-right" type="button" onClick={() => eventDeleteDoc()} >
-                            Supprimer
+                        <Button variant="" className="float-right" type="button" onClick={() => onChangeHandler(null)} >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash">
+                                <polyline points="3 6 5 6 21 6"></polyline>
+                                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                            </svg>
                         </Button>
                     } 
                 </p>                               
