31578320aa437a38d48f9e06adf12cf5dc641008
Maxime <maxime.derame@hotmail.fr>
2022-02-21 22:59:54 +0100

    fix generation

--- components/TypePermanence/DisplayButton.tsx
+++ components/TypePermanence/DisplayButton.tsx
@@ -1,107 +1,161 @@
 import React, { Component, useState, useEffect } from "react";
 import axios from "axios";
 import {
-    NotificationContainer,
-    NotificationManager,
-  } from "react-notifications";
-  import { nanoid } from "nanoid";
-  import Link from "next/link";
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
+import { nanoid } from "nanoid";
+import Link from "next/link";
 
 export default function Button(props) {
+  const [planning, setPlanning] = useState();
+  const [avocats, setAvocats] = useState([]);
 
-    const [planning, setPlanning] = useState();
-    const [avocats, setAvocats] = useState([]);
+  const { setPermanences, id } = props;
 
-    const { setPermanences, id } = props;
+  function generatePlanning(idPermanence) {
+    /// Quand il y a moins d'avocat que de date
+    let newEvent;
+    let dateEvent = new Date(2022, 5, 1);
 
-    function generatePlanning(idPermanence) {
-        /// Quand il y a moins d'avocat que de date
-        let newEvent;
-        let dateEvent = new Date(2022, 5, 1);
-    
-        const planningId = nanoid();
-    
-    
-        let newPlanning = {
-          month: +props.month + 1,
-          year: +props.year,
-          createdAt: new Date(),
-          permanenceId: +idPermanence,
-          id: planningId,
+    const planningId = nanoid();
+
+    let newPlanning = {
+      month: +props.month + 1,
+      year: +props.year,
+      createdAt: new Date(),
+      permanenceId: +idPermanence,
+      id: planningId,
+    };
+
+    axios.post("/api/planning", newPlanning).then(function (response) {
+      let lastDay = new Date(+props.year, +props.month + 1, 0).getDate();
+      let firstDay = new Date(+props.year, +props.month + 1, 1).getDate();
+
+      try {
+        for (let index = firstDay; index <= lastDay; index++) {
+          let avocat_id =
+            avocats[Math.floor(Math.random() * avocats.length)]["id"];
+
+          newEvent = {
+            date: new Date(props.year, props.month, index),
+            planning_id: planningId,
+            user_id: +avocat_id,
+          };
+
+          axios.post("/api/event", newEvent).then(function (response) {
+            axios
+              .get("/api/planning", {
+                params: {
+                  month: props.month + 1,
+                  year: props.year,
+                  permanenceId: props.permanenceId,
+                },
+              })
+              .then(function (response) {
+                setPlanning(response.data);
+              });
+            axios
+              .get("/api/permanence", { params: { typePermanence_id: id } })
+              .then(function (response) {
+                setPermanences(response.data);
+              });
+          });
         }
-    
-        axios.post("/api/planning", newPlanning)
-           .then(function(response) {
-             for (let index = 0; index < avocats.length; index++) {
-               let avocat_id = avocats[index]["id"];
-        
-               newEvent = {
-                 date: new Date(2022, 5, index++),
-                 planning_id: planningId,
-                 user_id: +avocat_id
-               };
-        
-               axios
-                 .post("/api/event", newEvent)
-                 .then(function (response) {
-                   NotificationManager.success(
-                     "success",
-                     "Le planning a été généré avec succès.",
-                     3000
-                   );
-                   axios
-                    .get("/api/planning", {params: {month: props.month + 1, year: props.year, permanenceId: props.permanenceId}})
-                    .then(function (response) {
-                      setPlanning(response.data);
-                    });
-                   axios
-                    .get("/api/permanence", { params: { typePermanence_id: id } })
-                    .then(function (response) {
-                      setPermanences(response.data);
-                    });
 
-                 })
-                 .catch(function (error) {
-                   NotificationManager.error(
-                     "warning",
-                     "Une erreur est survenue lors de la génération du planning. Si le problème persiste, veuillez contacter le support.",
-                     3000
-                   );
-                 });
-             }
-           })
-    
-        
+        NotificationManager.success(
+          "success",
+          "La planning a été généré avec succès.",
+          3000
+        );
+      } catch (error) {
+        NotificationManager.error(
+          "warning",
+          "Une erreur est survenue lors de la génération du planning. Si le problème persiste, veuillez contacter le support.",
+          3000
+        );
       }
 
-    useEffect(() => {
-        axios
-          .get("/api/planning", {params: {month: props.month + 1, year: props.year, permanenceId: props.permanenceId}})
-          .then(function (response) {
-            setPlanning(response.data);
-          });
+      /// Affecter un avocat par date
+      // for (let index = 0; index < avocats.length; index++) {
+      //   let avocat_id = avocats[index]["id"];
 
-          axios
-          .get("/api/user", { params: { role_id: 2 } })
-          .then(function (response) {
-            setAvocats(response.data);
-          });
-    
-      }, [props.month]);
+      //   newEvent = {
+      //     date: new Date(2022, 5, index++),
+      //     planning_id: planningId,
+      //     user_id: +avocat_id,
+      //   };
 
-    return (
+      //   axios
+      //     .post("/api/event", newEvent)
+      //     .then(function (response) {
+      //       axios
+      //         .get("/api/planning", {
+      //           params: {
+      //             month: props.month + 1,
+      //             year: props.year,
+      //             permanenceId: props.permanenceId,
+      //           },
+      //         })
+      //         .then(function (response) {
+      //           setPlanning(response.data);
+      //           NotificationManager.success(
+      //             "success",
+      //             "Le planning a été généré avec succès.",
+      //             3000
+      //           );
+      //         });
+      //       axios
+      //         .get("/api/permanence", { params: { typePermanence_id: id } })
+      //         .then(function (response) {
+      //           setPermanences(response.data);
+      //         });
+      //     })
+      //     .catch(function (error) {
+      //       NotificationManager.error(
+      //         "warning",
+      //         "Une erreur est survenue lors de la génération du planning. Si le problème persiste, veuillez contacter le support.",
+      //         3000
+      //       );
+      //     });
+      // }
+    });
+  }
 
+  useEffect(() => {
+    axios
+      .get("/api/planning", {
+        params: {
+          month: props.month + 1,
+          year: props.year,
+          permanenceId: props.permanenceId,
+        },
+      })
+      .then(function (response) {
+        setPlanning(response.data);
+      });
 
+    axios
+      .get("/api/user")
+      .then(function (response) {
+        setAvocats(response.data);
+      });
+  }, [props.month]);
 
-        <>{planning ? <Link href={`/app/admin/planning/${planning.id}`}>
-               <a className="btn btn-dark">
-                 Afficher le planning
-               </a>
-               </Link> : <button
-                   onClick={() => generatePlanning(props.permanenceId)}
-                   className="btn btn-dark"
-                 >
-                   Générer le planning
-                 </button>}</>
-    )
+  return (
+    <>
+      {planning ? (
+        <Link href={`/app/admin/planning/${planning.id}`}>
+          <a className="btn btn-dark">Afficher le planning</a>
+        </Link>
+      ) : (
+        <button
+          onClick={() => generatePlanning(props.permanenceId)}
+          className="btn btn-dark"
+        >
+          Générer le planning
+        </button>
+      )}
+    </>
+  );
 }
